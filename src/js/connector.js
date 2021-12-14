02fcbe83a8e06d2ef4f2801109f695cc5cdfe1c9
yulin <yulin.chen@thoughtworks.com>
2021-12-14 10:37:19 +0800

    [yulin] feat: load card for card-badges capacity

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,12 @@ console.log("hello world!")
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
         // return an array of card badges
-        return [];
+        return t.card('all')
+            .then(function(card) {
+                console.log(card);
+                return [{
+                    text: card.idShort;
+                }];
+            })
     },
 });
