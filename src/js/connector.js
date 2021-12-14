2dda84171a47494dafc27e5430a2d9473baba46d
yulin <yulin.chen@thoughtworks.com>
2021-12-14 15:21:44 +0800

    [yulin] feat: test a board button

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,34 @@
 console.log("hello world!")
 
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+var onBtnClick = function (t, opts) {
+    console.log('Someone clicked the button');
+};
+
 window.TrelloPowerUp.initialize({
-    "card-badges": function (t, opts) {
-        // return an array of card badges
-        return t.card('all')
-            .then(function(card) {
-                console.log(card);
-                return [{
-                    text: card.idShort
-                }];
-            })
-    },
+    'board-buttons': function (t, opts) {
+        return [{
+            // we can either provide a button that has a callback function
+            icon: {
+                dark: WHITE_ICON,
+                light: BLACK_ICON
+            },
+            text: 'Callback',
+            callback: onBtnClick,
+            condition: 'edit'
+        }, {
+            // or we can also have a button that is just a simple url
+            // clicking it will open a new tab at the provided url
+            icon: {
+                dark: WHITE_ICON,
+                light: BLACK_ICON
+            },
+            text: 'URL',
+            condition: 'always',
+            url: 'https://trello.com/inspiration',
+            target: 'Inspiring Boards' // optional target for above url
+        }];
+    }
 });
