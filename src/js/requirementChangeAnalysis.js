03563869851f3e63808dc3e4b838a13f4c632143
yulin <yulin.chen@thoughtworks.com>
2021-12-22 23:36:44 +0800

    [yulin] feat: test diff

--- src/js/requirementChangeAnalysis.js
+++ src/js/requirementChangeAnalysis.js
@@ -1,10 +1,29 @@
-const echarts = require("echarts");
-
 let cardsInfo = [];
 let labelSet = [];
 let dataSet = {};
 
 const t = window.TrelloPowerUp.iframe();
+
+var echarts = require('echarts');
+var chartDom = document.getElementById('charts');
+var myChart = echarts.init(chartDom);
+var option;
+option = {
+    dataset: {
+        dimensions: ['product', '2015', '2016', '2017'],
+        source: [
+            { product: 'Matcha Latte', 2015: 43.3, 2016: 85.8, 2017: 93.7 },
+            { product: 'Milk Tea', 2015: 83.1, 2016: 73.4, 2017: 55.1 },
+            { product: 'Cheese Cocoa', 2015: 86.4, 2016: 65.2, 2017: 82.5 },
+            { product: 'Walnut Brownie', 2015: 72.4, 2016: 53.9, 2017: 39.1 }
+        ]
+    },
+    xAxis: { type: 'category' },
+    yAxis: {},
+    series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+};
+option && myChart.setOption(option);
+
 
 t.board('labels').then(res => {
     const _ = require('lodash');
@@ -34,29 +53,11 @@ startAnalysis = () => {
     console.log('source: ', source);
     console.log('labels: ', labels);
 
-    var echarts = require('echarts');
-    var chartDom = document.getElementById('charts');
-    var myChart = echarts.init(chartDom);
-    var option;
-    option = {
-        dataset: {
-            dimensions: ['product', '2015', '2016', '2017'],
-            source: [
-                { product: 'Matcha Latte', 2015: 43.3, 2016: 85.8, 2017: 93.7 },
-                { product: 'Milk Tea', 2015: 83.1, 2016: 73.4, 2017: 55.1 },
-                { product: 'Cheese Cocoa', 2015: 86.4, 2016: 65.2, 2017: 82.5 },
-                { product: 'Walnut Brownie', 2015: 72.4, 2016: 53.9, 2017: 39.1 }
-            ]
-        },
-        xAxis: { type: 'category' },
-        yAxis: {},
-        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
-    };
     option.dataset.dimensions = ['label', ...labels];
     option.dataset.source = source;
     option.series = [{ type: 'bar' }, { type: 'bar' }];
-    option && myChart.setOption(option);
     console.log('option: ', option);
+    myChart.setOption(option);
 }
 
 calculateRequirementChangeCountAsSource = dataSet => {
