8777da1aeb6b41b2426d62a488dba38ca5e757a5
yulin <yulin.chen@thoughtworks.com>
2021-12-23 21:35:41 +0800

    [yulin] feat: test diff

--- src/js/requirementChangeAnalysis.js
+++ src/js/requirementChangeAnalysis.js
@@ -90,18 +90,16 @@ generateHistogramOption = source => {
         tooltip: {},
         dataset: {
             source: [
-                ['product', '2015', '2016', '2017'],
-                ['Matcha Latte', 43.3, 85.8, 93.7],
-                ['Milk Tea', 83.1, 73.4, 55.1],
-                ['Cheese Cocoa', 86.4, 65.2, 82.5],
-                ['Walnut Brownie', 72.4, 53.9, 39.1]
+                ['cycle', 'cards count', 'changes count'],
+                ['cycle1', 7, 17],
+                ['cycle2', 3, 34],
             ]
         },
         xAxis: { type: 'category' },
         yAxis: {},
         // Declare several bar series, each will be mapped
         // to a column of dataset.source by default.
-        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+        series: [{ type: 'bar' }, { type: 'bar' }]
     };
     histogramOption.dataset.source = source;
     return histogramOption;
